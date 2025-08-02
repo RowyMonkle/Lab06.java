747bb59fbae2a805b187bfb7b2e946c7ace2ab4b
RowyMonke <nattakritta_ao@cmu.ac.th>
2025-08-03 04:26:58 +0700

    editing some code and adding more addition comment

--- FlyingShark.java
+++ FlyingShark.java
@@ -6,11 +6,12 @@ public class FlyingShark extends Shark implements Flyable{
 
     @Override
     public void eat(Fish fish) {
-        super.eat(fish);
-        System.out.println("FlyingShark caught a flying creature!");
+        super.eat(fish); //Calls the eat(Fish) method from the superclass (extends class: Shark)
+        System.out.println(this.toString() + "eaten by" + fish.toString()); //display Fish fish() that eaten by FlyingShark
     }
     @Override
-    public void eat(Flyable winged) {
-        System.out.println("Flying Shark is gliding through the air! (I know it's impossible but who cares!)");
+    public void eat(Flyable winged) {//Adding more eat() for help FlyingShark eat the flying creature
+        System.out.println("FlyingShark caught a flying creature!");
     }
 }
+
